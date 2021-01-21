9d0d311b1b79f3ada9e5bdb443454f9ae26f211f
dilane3 <komboudilane125@gmail.com>
2021-01-21 13:37:43 +0100

    creation d'un fichier javascript pour la gestion de la structure de base des l'interface

---
+++ js/screenHadler.js
@@ -0,0 +1,44 @@
+let screen = document.querySelector(".container-fluid");
+let columnInterface = [...document.querySelectorAll(".column")];
+let messagesInterface = document.querySelector(".messages");
+let contactsList = document.querySelector(".contacts-list");
+let conversation = document.querySelector(".conversation");
+let screenWidth = window.innerWidth;
+
+screen.style.height = `${window.innerHeight}px`;
+screen.style.width = `${window.innerWidth}px`;
+
+columnInterface.forEach(column => {
+  column.style.height = `${window.innerHeight}px`;
+});
+
+messagesInterface.style.height = `${window.innerHeight - 150}px`;
+
+if (screenWidth < 768) {
+  contactsList.style.height = `${window.innerHeight - 150}px`;
+} else {
+  contactsList.style.height = `${window.innerHeight - 80}px`;
+}
+
+if (screenWidth <= 575) {
+  conversation.classList.add("d-block");
+}
+
+window.onresize = function () {
+  screenWidth = window.innerWidth;
+
+  screen.style.height = `${window.innerHeight}px`;
+  screen.style.width = `${window.innerWidth}px`;
+
+  columnInterface.forEach(column => {
+    column.style.height = `${window.innerHeight}px`;
+  });
+
+  if (screenWidth < 768) {
+    contactsList.style.height = `${window.innerHeight - 150}px`;
+  } else {
+    contactsList.style.height = `${window.innerHeight - 80}px`;
+  }
+  messagesInterface.style.height = `${window.innerHeight - 150}px`;
+}
+
